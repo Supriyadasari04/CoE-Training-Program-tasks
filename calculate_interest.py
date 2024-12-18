5261d1251beac77ca9ab1520b6630c6d4f787313
Supriya Dasari <dasarisupriya3012@gmail.com>
2024-12-18 13:59:38 +0530

    Add files via upload

---
+++ calculate_interest.py
@@ -0,0 +1,5 @@
+k0 = float(input("Enter the initial balance : "))
+interest_rate =  float(input("Enter the interest rate : "))
+time = 1
+k1 = k0 *(1 + interest_rate)
+print("New capital k1 after one year including interest is : ",k1)
