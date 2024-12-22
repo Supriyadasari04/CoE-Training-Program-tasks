a5766db67b37fb9248779a238979051ebc6e7683
Supriya Dasari <dasarisupriya3012@gmail.com>
2024-12-22 07:27:59 +0000

    Commit

---
+++ Module tasks/animal_counts.py
@@ -0,0 +1,17 @@
+def accumulate(dicts, key):
+    total = 0
+    for d in dicts:
+        try:
+            total += d[key]  
+        except KeyError:
+            pass
+    return total
+d = [
+    {"Puppies": 17, "Kittens": 9, "Birds": 23, "Fish": 90, "Hamsters": 49},
+    {"Puppies": 23, "Birds": 29, "Fish": 20, "Mice": 20, "Snakes": 7},
+    {"Fish": 203, "Hamsters": 93, "Snakes": 25, "Kittens": 89},
+    {"Birds": 20, "Puppies": 90, "Snakes": 21, "Fish": 10, "Kittens": 67},
+]
+key = "Puppies"
+total = accumulate(d, key)
+print(f"Total number of {key.lower()}: {total}")
